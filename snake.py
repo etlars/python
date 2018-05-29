7b26b5e12433b7c0bf4ec61e79b4fd93ec9f81eb
etlars <etlars@gmail.com>
2018-05-29 22:11:33 +0900

    add comment

--- snake.py
+++ snake.py
@@ -8,6 +8,7 @@ def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 	else:
 		return False
 
+# I will move die function to anywhere.
 def die(screen, score):
 	f=pygame.font.SysFont('Arial', 30)
 	t=f.render('Your score was: '+str(score), True, (0, 0, 0))
