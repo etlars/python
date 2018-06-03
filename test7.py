19b8083cf3454c5abedd8f55f0c22f5de9c3fe4e
etlars <etlars@gmail.com>
2018-06-03 19:47:56 +0900

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,99 +1,46 @@
 #coding=uft-8
-import pygame, random, sys
-#import time #1. controll speed
-from pygame.locals import *
+import pygame
 
-def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 
-    if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
-        return True
-    else:
-        return False
-
-# I will move die function to anywhere.
-def die(screen, score):
-    f=pygame.font.SysFont('Arial', 30)
-    t=f.render('Your score was: '+str(score), True, (0, 0, 0))
-    screen.blit(t, (10, 270))
-    pygame.display.update()
-    pygame.time.wait(2000)
-    pygame.time.wait(100)
-    sys.exit(0)
-
-xs = [290, 290, 290, 290, 290]
-ys = [290, 270, 250, 230, 210]
-dirs = 0
-score = 0
-applepos = (random.randint(0, 590), random.randint(0, 590))
 pygame.init()
-s=pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Snake')
 
-appleimage = pygame.Surface((10, 10))
-appleimage.fill((0, 255, 0))
-img = pygame.Surface((20, 20))
-img.fill((255, 0, 0))
+X_SCREENSIZE=800
+Y_SCREENSIZE=600
+Tick = 60
 
-f = pygame.font.SysFont('Arial', 20)
-clock = pygame.time.Clock()
+ourScreen=pygame.display.set_mode((X_SCREENSIZE, Y_SCREENSIZE))
+pygame.display.set_caption("파이게임")
+finish = False;
+color3=(0,0,0)
 
-while True:
-    clock.tick(10)  #2. decrease it to slow down 
+img = pygame.image.load("zzang.png")
+
+def myimg(x, y):
+    ourScreen.blit(img, (x, y))
+
+x=X_SCREENSIZE*0.5
+y=Y_SCREENSIZE*0.5
+
+#clock = pygame.time.Clock()
+
+while not finish:
     for e in pygame.event.get():
-        if e.type == QUIT:
-            sys.exit(0)
-        elif e.type == KEYDOWN:
-            if e.key == K_UP and dirs != 0:
-                dirs = 2
-            elif e.key == K_DOWN and dirs != 2:
-                dirs = 0
-            elif e.key == K_LEFT and dirs != 1:
-                dirs = 3
-            elif e.key == K_RIGHT and dirs != 3:
-                dirs = 1
-    i = len(xs)-1
+        if e.type == pygame.QUIT:
+            finish = True
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE : finish = True
+            elif e.key == pygame.K_UP   : y-=Tick
+            elif e.key == pygame.K_DOWN : y+=Tick
+            elif e.key == pygame.K_LEFT : x-=Tick
+            elif e.key == pygame.K_RIGHT: x+=Tick
+        
+        if y < 0: y=0
+        if y > Y_SCREENSIZE: y=Y_SCREENSIZE
+        if x < 0: x=0
+        if x > X_SCREENSIZE: x=X_SCREENSIZE
 
-    while i >= 2:
-        if collide(xs[0], xs[i], ys[0], ys[i], 20, 20, 20, 20):
-            die(s, score)
-        i-= 1
-
-    if collide(xs[0], applepos[0], ys[0], applepos[1], 20, 10, 20, 10):
-        score+=1
-        xs.append(700)
-        ys.append(700)
-        applepos=(random.randint(5,585),random.randint(5,585))
-
-    if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: 
-        die(s, score)
-
-    i = len(xs)-1
-
-    while i >= 1:
-        xs[i] = xs[i-1]
-        ys[i] = ys[i-1]
-        i -= 1
-
-    if dirs==0:
-        ys[0] += 20
-    elif dirs==1:
-        xs[0] += 20
-    elif dirs==2:
-        ys[0] -= 20
-    elif dirs==3:
-        xs[0] -= 20    
-
-    s.fill((255, 255, 255))    
-
-    for i in range(0, len(xs)):
-        s.blit(img, (xs[i], ys[i]))
-
-    s.blit(appleimage, applepos)
-
-    t=f.render(str(score), True, (0, 0, 0))
-    s.blit(t, (10, 10))
-    
-    
-        #pygame.time.wait(100) #3. uncomment if controll speed
-    #time.sleep(0.1)
-    pygame.display.update()
+        ourScreen.fill(color3)
+        myimg(x, y)
+   
+        
+        pygame.display.update()
