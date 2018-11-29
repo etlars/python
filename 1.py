1eb24c63d2b71d77a7e1181d5308640884863622
etlars <etlars@gmail.com>
2018-11-30 02:13:23 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,35 +1,11 @@
+# -*- coding:utf-8 -*-
 
+from functools import reduce
 
-
-from tkinter import *
-
-root = Tk()
-
-red = IntVar()
-red.set(0)
-blue= IntVar()
-blue.set(0)
-green= IntVar()
-green.set(0)
-
-
-def change_color(n):
-    color='#%02x%02x%02x'%(red.get(),blue.get(), green.get())
-    button.configure(bg=color)
-
-
-button = Button(root, text = 'button', bg = 'pink')
-button.pack(fill='both')
-
-
-s1= Scale(root, label = 'red', orient = 'v', from_=0, to=255, variable=red, command=change_color)
-s2= Scale(root, label = 'blue', orient = 'v', from_=0, to=255, variable=blue, command=change_color)
-s3= Scale(root, label = 'green', orient = 'v', from_=0, to=255, variable=green, command=change_color)
-
-
-s1.pack(fill='both')
-s2.pack(fill='both')
-s3.pack(fill='both')
-
-
-root.mainloop();
+def Result(x , y) :
+    return x if x<y else y
+    
+    
+l = [ 8 , 9 , 9 , 4 , 5 , 9, 3  , 2 , 1 ]
+ret =reduce(Result,l)
+print (ret)
