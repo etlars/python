87d62734c852231dad8dd5a1b57ae6127ba2e94b
etlars <etlars@gmail.com>
2018-11-30 00:53:52 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -20,8 +20,14 @@ def change_bg(item, color):
     
 class MyWindow(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)    
-        
+        #tk.Frame.__init__(self, master)    
+        super(MyWindow, self).__init__(master)
+        self.width=300
+        self.height=0
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height)
+        #self.canvas = tk.Canvas(self)
+        self.canvas.pack()
+        self.pack() 
         
         info = tk.Label(self, text=u'색상 변화를 보세요 ', font=('궁서체', '12', 'bold'))
         info.pack(padx=10, pady=5)
@@ -37,7 +43,8 @@ class MyWindow(tk.Frame):
             self.sc[color] = tk.Scale(f, from_=0, to=255, orient='v', command=self.M_change)
             self.sc[color].set(255)
             self.sc[color].grid(row=1, column=i, padx=10, pady=5)
-            
+        
+           
 
     def M_change(self, event):
         color = '#%02X%02X%02X' % (self.sc['red'].get(), self.sc['green'].get(), self.sc['blue'].get())
@@ -47,16 +54,6 @@ class MyWindow(tk.Frame):
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.wm_title('tk')
-    
+    root.title('tk')
     app = MyWindow(root)
-    
-    #root.geometry("500x300+0+0")
-    
     root.mainloop()
-
-
-
-#https://m.blog.naver.com/PostView.nhn?blogId=dudwo567890&logNo=130168372508&proxyReferer=https%3A%2F%2Fwww.google.co.kr%2F
-#https://076923.github.io/posts/Python-tkinter-19/
-#https://gist.github.com/withover/de66642df13cd8e1c5e6117f35001444
